e960cce3c7954015243c9c9e67757b8171261ebc
Ivet Lopez <ivet19@live.com>
2025-04-17 19:26:45 +0200

    Create NotFoundPage and add styles

---
+++ src/pokemon/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,22 @@
+import "./NotFoundPage.css";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="principal-container">
+      <div className="error-container">
+        <h1 className="error-code">404</h1>
+        <p className="error-message">Page not found</p>
+      </div>
+      <img
+        src="404_pikachu.png"
+        alt="Pikachu with a detective hat and a hand lens"
+        width={300}
+      />
+      <p className="error-message">
+        The page you are looking for doesn't exist or an error has ocurred.
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
