f68cff6762aa0661d715e8df6e6cb260c2e16501
Ivet Lopez <ivet19@live.com>
2025-04-20 20:55:36 +0200

    Add addPokemon method to PokemonsClientStructure

--- src/pokemon/client/types.ts
+++ src/pokemon/client/types.ts
@@ -1,5 +1,8 @@
 import { PokemonDto } from "../dto/types";
+import { PokemonData } from "../types";
+import { Pokemon } from "../types";
 
 export interface PokemonsClientStructure {
   getPokemons: () => Promise<PokemonDto[]>;
+  addPokemon: (pokemonData: PokemonData) => Promise<Pokemon>;
 }
