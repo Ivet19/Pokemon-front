8059f56d7bd4bbe9430f42d791181ca76c5996c4
Ivet Lopez <ivet19@live.com>
2025-04-20 20:57:40 +0200

    Create createPokemon method in Provider

--- src/pokemon/context/types.ts
+++ src/pokemon/context/types.ts
@@ -1,8 +1,9 @@
-import { Pokemon } from "../types";
+import { Pokemon, PokemonData } from "../types";
 
 interface PokemonContextStructure {
   pokemons: Pokemon[];
   loadPokemons: () => Promise<void>;
+  createPokemon: (pokemonData: PokemonData) => Promise<void>;
 }
 
 export default PokemonContextStructure;
